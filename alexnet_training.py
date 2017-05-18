c4166eea3f13e49bdf6fc9e8a19b1b8166983b0b
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-18 22:03:02 +0100

    auto git commit

--- alexnet_training.py
+++ alexnet_training.py
@@ -81,21 +81,21 @@ def initialize_weights_mask(first_time_training, mask_dir, file_name):
     if (first_time_training):
         print('setting initial mask value')
         weights_mask = {
-            'cov1': np.ones([11, 11, NUM_CHANNELS, 96]),
-            'cov2': np.ones([5, 5, 96, 256]),
-            'cov3': np.ones([3, 3, 256, 384]),
-            'cov4': np.ones([3, 3, 384, 384]),
-            'cov5': np.ones([3, 3, 384, 256]),
+            'conv1': np.ones([11, 11, NUM_CHANNELS, 96]),
+            'conv2': np.ones([5, 5, 96, 256]),
+            'conv3': np.ones([3, 3, 256, 384]),
+            'conv4': np.ones([3, 3, 384, 384]),
+            'conv5': np.ones([3, 3, 384, 256]),
             'fc6': np.ones([6 * 6 * 256, 4096]),
             'fc7': np.ones([4096, 4096]),
             'fc8': np.ones([4096, NUM_CLASSES])
         }
         biases_mask = {
-            'cov1': np.ones([64]),
-            'cov2': np.ones([64]),
-            'cov3': np.ones([64]),
-            'cov4': np.ones([64]),
-            'cov5': np.ones([64]),
+            'conv1': np.ones([64]),
+            'conv2': np.ones([64]),
+            'conv3': np.ones([64]),
+            'conv4': np.ones([64]),
+            'conv5': np.ones([64]),
             'fc6': np.ones([384]),
             'fc7': np.ones([192]),
             'fc8': np.ones([NUM_CLASSES])
