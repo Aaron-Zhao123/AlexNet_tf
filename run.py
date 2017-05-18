e6739108dac85af6d2f74a7c76d38f3fd6bd4af1
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-18 20:04:30 +0100

    auto git commit

--- run.py
+++ run.py
@@ -71,7 +71,7 @@ while (crates['fc1'] < 2):
             ('-lambda1', 1e-5),
             ('-lambda2', 1e-5)
             ]
-        _ = train.main(param)
+        _ = alexnet_training.main(param)
 
         # TEST
         param = [
