163a1c1bfe639e21db0444a812df659cdd530f40
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-19 10:15:43 +0100

    auto git commit

--- alexnet.py
+++ alexnet.py
@@ -165,10 +165,10 @@ def fc(x, num_in, num_out, name, mask, relu = True):
 
     # Create tf variables for the weights and biases
     weights = get_scope_variable(scope, 'weights',
-            shape = [filter_height, filter_width, input_channels/groups, num_filters],
+            shape=[num_in, num_out],
             initializer = tf.truncated_normal_initializer())
     biases = get_scope_variable(scope, 'biases',
-            shape = [num_filters],
+            shape = [num_out],
             initializer=tf.random_normal_initializer())
     # weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
     # biases = tf.get_variable('biases', [num_out], trainable=True)
