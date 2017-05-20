4b1d71cc3157cbf4708423f21ef3af9831ab9066
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-20 11:35:45 +0100

    auto git add all

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-import alexnet_training
+import alexnet_training_simple
 import sys
 import download
 
@@ -51,92 +51,92 @@ if (INITIAL_TRAIN):
         ('-lambda1', 1e-5),
         ('-lambda2', 1e-5)
         ]
-    _ = alexnet_training.main(param)
+    _ = alexnet_training_simple.main(param)
     sys.exit()
 
-
-# Prune
-while (crates['fc1'] < 2):
-    count = 0
-    iter_cnt = 0
-    while (iter_cnt < 7):
-        if (iter_cnt > 3 and iter_cnt < 5):
-            lr = 5e-5
-        elif (iter_cnt >= 5):
-            lr = 1e-5
-        else:
-            lr = 1e-4
-        param = [
-            ('-cRates', crates),
-            ('-first_time', False),
-            ('-train', False),
-            ('-prune', True),
-            ('-lr', lr),
-            ('-with_biases', with_biases),
-            ('-parent_dir', parent_dir),
-            ('-lambda1', 1e-5),
-            ('-lambda2', 1e-5)
-            ]
-        # _ = train.main(param)
-
-        # TRAIN
-        param = [
-            ('-cRates', crates),
-            ('-first_time', False),
-            ('-train', True),
-            ('-prune', False),
-            ('-lr', lr),
-            ('-with_biases', with_biases),
-            ('-parent_dir', parent_dir),
-            ('-lambda1', 1e-5),
-            ('-lambda2', 1e-5)
-            ]
-        _ = alexnet_training.main(param)
-
-        # TEST
-        param = [
-            ('-cRates', crates),
-            ('-first_time', False),
-            ('-train', False),
-            ('-prune', False),
-            ('-test', True),
-            ('-lr', lr),
-            ('-with_biases', with_biases),
-            ('-parent_dir', parent_dir),
-            ('-lambda1', 1e-5),
-            ('-lambda2', 1e-5)
-            ]
-        acc = alexnet_training.main(param)
-
-
-        if (acc > 0.823 or iter_cnt == 7):
-            file_name = compute_file_name(crates)
-            crates['fc1'] = crates['fc1'] + 0.05
-            # crates['cov2'] = crates['cov2'] + 0.2
-            # crates['fc2'] = crates['fc2'] + 0.5
-            # crates['fc3'] = crates['fc3'] + 0.1
-            # crates['cov2'] = crates['cov2'] + 0.5
-            # crates['cov1'] = crates['cov1'] + 0.2
-            acc_list.append((crates,acc))
-            param = [
-                ('-first_time', False),
-                ('-train', False),
-                ('-prune', False),
-                ('-lr', lr),
-                ('-with_biases', with_biases),
-                ('-parent_dir', parent_dir),
-                ('-iter_cnt',iter_cnt),
-                ('-cRates',crates),
-                ('-save', True),
-                ('-lambda1', 1e-5),
-                ('-lambda2', 1e-5),
-                ('-org_file_name', file_name)
-                ]
-            _ = train.main(param)
-            break
-        else:
-            iter_cnt = iter_cnt + 1
-    print('accuracy summary: {}'.format(acc_list))
-
-
-print('accuracy summary: {}'.format(acc_list))
+#
+# # Prune
+# while (crates['fc1'] < 2):
+#     count = 0
+#     iter_cnt = 0
+#     while (iter_cnt < 7):
+#         if (iter_cnt > 3 and iter_cnt < 5):
+#             lr = 5e-5
+#         elif (iter_cnt >= 5):
+#             lr = 1e-5
+#         else:
+#             lr = 1e-4
+#         param = [
+#             ('-cRates', crates),
+#             ('-first_time', False),
+#             ('-train', False),
+#             ('-prune', True),
+#             ('-lr', lr),
+#             ('-with_biases', with_biases),
+#             ('-parent_dir', parent_dir),
+#             ('-lambda1', 1e-5),
+#             ('-lambda2', 1e-5)
+#             ]
+#         # _ = train.main(param)
+#
+#         # TRAIN
+#         param = [
+#             ('-cRates', crates),
+#             ('-first_time', False),
+#             ('-train', True),
+#             ('-prune', False),
+#             ('-lr', lr),
+#             ('-with_biases', with_biases),
+#             ('-parent_dir', parent_dir),
+#             ('-lambda1', 1e-5),
+#             ('-lambda2', 1e-5)
+#             ]
+#         _ = alexnet_training.main(param)
+#
+#         # TEST
+#         param = [
+#             ('-cRates', crates),
+#             ('-first_time', False),
+#             ('-train', False),
+#             ('-prune', False),
+#             ('-test', True),
+#             ('-lr', lr),
+#             ('-with_biases', with_biases),
+#             ('-parent_dir', parent_dir),
+#             ('-lambda1', 1e-5),
+#             ('-lambda2', 1e-5)
+#             ]
+#         acc = alexnet_training.main(param)
+#
+#
+#         if (acc > 0.823 or iter_cnt == 7):
+#             file_name = compute_file_name(crates)
+#             crates['fc1'] = crates['fc1'] + 0.05
+#             # crates['cov2'] = crates['cov2'] + 0.2
+#             # crates['fc2'] = crates['fc2'] + 0.5
+#             # crates['fc3'] = crates['fc3'] + 0.1
+#             # crates['cov2'] = crates['cov2'] + 0.5
+#             # crates['cov1'] = crates['cov1'] + 0.2
+#             acc_list.append((crates,acc))
+#             param = [
+#                 ('-first_time', False),
+#                 ('-train', False),
+#                 ('-prune', False),
+#                 ('-lr', lr),
+#                 ('-with_biases', with_biases),
+#                 ('-parent_dir', parent_dir),
+#                 ('-iter_cnt',iter_cnt),
+#                 ('-cRates',crates),
+#                 ('-save', True),
+#                 ('-lambda1', 1e-5),
+#                 ('-lambda2', 1e-5),
+#                 ('-org_file_name', file_name)
+#                 ]
+#             _ = train.main(param)
+#             break
+#         else:
+#             iter_cnt = iter_cnt + 1
+#     print('accuracy summary: {}'.format(acc_list))
+#
+#
+# print('accuracy summary: {}'.format(acc_list))
