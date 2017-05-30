510882ac7d3214048f67224ac5647d84b7f41164
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-30 11:06:27 +0100

    auto git add all

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ if (INITIAL_TRAIN):
         ('-first_time', True),
         # ('-first_time', False),
         ('-train', False),
-        ('-test', False),
+        ('-test', True),
         ('-prune', False),
         # ('-test', True),
         ('-lr', lr),
