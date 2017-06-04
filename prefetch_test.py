25c6954cd4ecfa96479f8ea89494ad6046177c26
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-06-04 15:24:24 +0100

    auto git add all

--- prefetch_test.py
+++ prefetch_test.py
@@ -1,5 +1,5 @@
 from tensorpack import *
 PATH = '/local/scratch/share/ImageNet/ILSVRC/Data/CLS-LOC'
-ds = dataset.ILSVRC12(PATH, 'train', shuffle = False)
-ds = BatchData(ds, 256, use_list=True)
-TestDataSpeed(ds).start_test()
+ds0 = dataset.ILSVRC12(PATH, 'train', shuffle = True)
+ds1 = PrefetchDataZMQ(ds0, nr_proc=1)
+dftools.dump_dataflow_to_lmdb(ds1, './ILSVRC-train.lmdb')
